72bdc20f468befe7c992b9f20432e5d944c20864
Guilherme Cintra <cinguilherme@gmail.com>
2023-12-25 17:22:17 -0300

    upgrade docker compose

--- components/s3/src/gcc/platform/s3/core.clj
+++ components/s3/src/gcc/platform/s3/core.clj
@@ -1,6 +1,5 @@
 (ns gcc.platform.s3.core
   (:require 
-   [amazonica.core :as amazonica]
    [amazonica.aws.s3 :as s3]))
 
 
@@ -12,6 +11,11 @@
    :region     "us-east-1"})
  ; or whatever your LocalStack is configured to use
 
+(def miniio-credentials 
+  {:access-key "minio"
+   :secret-key "minio123"
+   :endpoint   "http://localhost:9000" ; miniio S3 endpoint
+   :path-style-access true})
 
 (comment
 
@@ -19,8 +23,12 @@
    aws-credentials
    "my-bucket")
 
-  
+  (s3/create-bucket
+   miniio-credentials
+   "my-bucket")
+
 
   (s3/list-buckets aws-credentials)
+  (s3/list-buckets miniio-credentials)
 
   )
