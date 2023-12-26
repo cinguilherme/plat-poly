8a35130530864dccf8abd97b819796e93a3ad864
Guilherme Cintra <cinguilherme@gmail.com>
2023-12-26 15:52:47 -0300

    add common and kafka components with dependencies

---
+++ components/common/test/gcc/platform/common/interface_test.clj
@@ -0,0 +1,6 @@
+(ns gcc.platform.common.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [gcc.platform.common.interface :as common]))
+
+(deftest dummy-test
+  (is (= 1 1)))
