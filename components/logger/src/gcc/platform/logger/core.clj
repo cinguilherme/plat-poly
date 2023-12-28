adf91bfb84940146b6b9f0655529d5adb7ce1be9
Guilherme Cintra <cinguilherme@gmail.com>
2023-12-28 13:31:36 -0300

    logger working

---
+++ components/logger/src/gcc/platform/logger/core.clj
@@ -0,0 +1,10 @@
+(ns gcc.platform.logger.core
+  (:require [clojure.tools.logging :as log]))
+
+(comment
+
+  (log/info "This is an informational message.")
+  
+  (log/error "This is an error message.")
+
+  )
